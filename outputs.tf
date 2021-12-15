4246fd7fccccbad06ac86ff7b31eea2227927e4b
Colin Wilson <colin.wilson@protonmail.ch>
2021-12-15 22:04:06 +0000

    ➕ add 'cluster_name' to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -17,6 +17,10 @@ output "cluster_id" {
   value = digitalocean_kubernetes_cluster.doks.id
 }
 
+output "cluster_name" {
+  value = var.cluster_name
+}
+
 output "cluster_vpc" {
   value = var.vpc_uuid == null ? data.digitalocean_vpc.doks.id : var.vpc_uuid
 }
